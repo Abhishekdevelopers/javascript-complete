7f313374703ff8a930048aac0b63f014f6bb5548
Abhishekdevelopers <142783859+Abhishekdevelopers@users.noreply.github.com>
2024-01-19 11:09:51 +0000

    memory stack heap

--- _1basics/datatypes-summary.js
+++ _1basics/datatypes-summary.js
@@ -4,11 +4,11 @@ const score = 100
 const scoreValue = 100.3
 const isLoggedIn = false
 const outsideTemp = null
-let userEmail;
-const id = Symbol('123')
-const anotherId = Symbole('123')
-const bigNumber = 3434364134664n
-console.log(id===anotherId);
+// let userEmail;
+// const id = symbol('123')
+// const anotherId = Symbole('123')
+// const bigNumber = 3434364134664n
+// console.log(id===anotherId);
 
 // Reference (Non primitive)
 
@@ -23,7 +23,28 @@ let myObj = {
 const details = function(){
     console.log("hello abhishek");
 }
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // viste the link if you want to know about the typeof different datatype 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// how to give the memory and use in Stack (Primitive), Heap (Non-Primitive)
+
+// it give only copy of the memory and child not change the value parent 
+let myYoutubename ="abhishek";
+let anothername = myYoutubename;
+anothername = "rishi"
+console.log(anothername);
+console.log(myYoutubename);
+
+// but you can change the value because both use the same refrence 
+
+let userOne = {
+    email:"user@googl.com",
+    upi:"user@qwe"
+}
+let userTwo = userOne
+userTwo.email = "abhishek@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
