26cf044001f831fe1338a74bdab76f516c266012
Abhishekdevelopers <142783859+Abhishekdevelopers@users.noreply.github.com>
2024-01-19 07:53:07 +0000

    conversion of datatype

--- _1basics/conversionOperation.js
+++ _1basics/conversionOperation.js
@@ -1,8 +1,8 @@
 let age= "26";
 let newage= Number(age);
-console.log(typeof(newage));
-console.log(typeof(age));
-console.log(age)
+// console.log(typeof(newage));
+// console.log(typeof(age));
+// console.log(age)
 
 // number, bigint, string, boolean, null, undefined, symbol
 
@@ -10,3 +10,21 @@ console.log(age)
 
 // data type of undefiend is undefiend 
 // data type of undefiend is object 
+
+const score = "33"
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+// "33"=>33
+// "33abc"=>NaN
+// true=>1; false=>0
+
+
+const isLogin = "abhishek";
+let changeIsLogin= Boolean(isLogin);
+console.log(changeIsLogin);
+console.log(isLogin);
+
+// 1 => true; 0=> false
+// ""=>false
+// "abhishek"=>true
