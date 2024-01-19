2ddd767c0b307af65cae13949635b8adc7e3a092
Abhishekdevelopers <142783859+Abhishekdevelopers@users.noreply.github.com>
2024-01-19 07:25:55 +0000

    hello

--- _1basics/conversionOperation.js
+++ _1basics/conversionOperation.js
@@ -3,3 +3,10 @@ let newage= Number(age);
 console.log(typeof(newage));
 console.log(typeof(age));
 console.log(age)
+
+// number, bigint, string, boolean, null, undefined, symbol
+
+// object
+
+// data type of undefiend is undefiend 
+// data type of undefiend is object 
