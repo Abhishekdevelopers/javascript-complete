8ac14f9cd8fc5c294f8d8f7ea11e131ece3fd9b8
Abhishekdevelopers <142783859+Abhishekdevelopers@users.noreply.github.com>
2024-01-19 10:34:06 +0000

    summary of data type

---
+++ _1basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+// 7 types: string, number, boolearn,null, undefined, Symbol, BigIn 
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = nulllet userEmail;
+const id = Symbol('123')
+const anotherId = Symbole('123')
+const bigNumber = 3434364134664n
+console.log(id===anotherId);
+
+// Reference (Non primitive)
+
+// Array, object, functions
+
+const heros = ["shaktiman", "abhishek", "Rishi"];
+
+let myObj = {
+    name:"abhishek",
+    age=25,
+}
+const details = function(){
+    console.log("hello abhishek");
+}
+console.log(typeof anotherId);
+
+// viste the link if you want to know about the typeof different datatype 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
